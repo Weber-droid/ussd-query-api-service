fa774b30b0cf2c5415f452e47b45416e96718ff1
Weber-droid <emmanueltotimeh2002@gmail.com>
2026-06-17 10:00:06 +0000

    Add initial project structure with Maven configuration and application files

---
+++ src/main/java/com/paicore/ussd_query_api_service/UssdQueryApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paicore.ussd_query_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UssdQueryApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UssdQueryApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paicore/ussd_query_api_service/UssdQueryApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paicore.ussd_query_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UssdQueryApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
